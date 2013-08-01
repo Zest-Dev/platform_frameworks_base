5174887fafde72dca1fb4f40efef4db3c83e45e5
franco-c <tjrdnjs33936@gmail.com>
2013-08-01 12:38:16 +0900

    Frameworks: CPU boosting Hooks && Interface
    Credits to CM
     * Boosts CPU using Power HAL for the given duration.
     * Duration is given in microseconds.
     * Power HAL must implement POWER_HINT_CPU_BOOST.
     * Boost CPU to max during scrolling
     * Boost CPU to max on activity start/resume
     * Inspired by a patch from CodeAuora.
    
    Change-Id: Id50847d9814e9ffc33c09c136406d0a8318bf3db

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -23,6 +23,7 @@ import com.android.internal.app.HeavyWeightSwitcherActivity;
 import com.android.internal.os.BatteryStatsImpl;
 import com.android.server.am.ActivityManagerService.PendingActivityLaunch;
 import com.android.server.wm.AppTransition;
+import com.android.server.power.PowerManagerService;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -57,6 +58,7 @@ import android.os.Message;
 import android.os.ParcelFileDescriptor;
 import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.util.EventLog;
@@ -319,6 +321,8 @@ final class ActivityStack {
 
     final Handler mHandler;
 
+    private final PowerManagerService mPm;
+
     final class ActivityStackHandler extends Handler {
         //public Handler() {
         //    if (localLOGV) Slog.v(TAG, "Handler started!");
@@ -436,6 +440,7 @@ final class ActivityStack {
             (PowerManager)context.getSystemService(Context.POWER_SERVICE);
         mGoingToSleep = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ActivityManager-Sleep");
         mLaunchingActivity = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ActivityManager-Launch");
+        mPm = (PowerManagerService) ServiceManager.getService("power");
         mLaunchingActivity.setReferenceCounted(false);
     }
 
@@ -1433,6 +1438,9 @@ final class ActivityStack {
     }
 
     final boolean resumeTopActivityLocked(ActivityRecord prev, Bundle options) {
+
+        mPm.cpuBoost(1500000);
+
         // Find the first activity that is not finishing.
         ActivityRecord next = topRunningActivityLocked(null);
 
@@ -2488,6 +2496,8 @@ final class ActivityStack {
             boolean componentSpecified, ActivityRecord[] outActivity) {
 
         int err = ActivityManager.START_SUCCESS;
+
+        mPm.cpuBoost(1500000);
 
         ProcessRecord callerApp = null;
 
